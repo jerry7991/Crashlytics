767ccb73460e8af29fb180823fb3a48f95a4ada4
jerry7991 <anup.s@browserstack.com>
2023-01-31 16:15:15 +0530

    crashlytic integration

--- Crashlytics/ContentView.swift
+++ Crashlytics/ContentView.swift
@@ -6,18 +6,23 @@
 //
 
 import SwiftUI
+import Crashlytics
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+//            Text("Hello, world!")
+//        }
+//        .padding()
+        Button("Crash") {
+          fatalError("Crash was triggered")
         }
-        .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
