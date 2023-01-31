f42cb2731b8d19c9b50576dfd498ce120e58f862
jerry7991 <anup.s@browserstack.com>
2023-01-31 12:16:40 +0530

    Initial Commit

---
+++ Crashlytics/CrashlyticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrashlyticsApp.swift
+//  Crashlytics
+//
+//  Created by Anup Kumar Singh on 31/01/23.
+//
+
+import SwiftUI
+
+@main
+struct CrashlyticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
